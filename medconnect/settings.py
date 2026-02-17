6b5c910133f79c7db5f7a1f6c7a2827ed873238a
cibinnnnnn <cibincinub@gmail.com>
2026-02-17 06:05:59 +0530

    Fixed ALLOWED_HOSTS for Render

--- medconnect/settings.py
+++ medconnect/settings.py
@@ -13,9 +13,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-medconnect-change-this-in-production'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com']
 
 
 # Application definition
